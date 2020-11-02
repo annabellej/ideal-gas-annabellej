05143ff974cbcc497d3cb4d75128549e4e7c31c2
annabellej <annabelleju02@gmail.com>
2020-11-01 20:12:45 -0800

    Added particle movement on each update.

--- include/simulator.h
+++ include/simulator.h
@@ -55,13 +55,14 @@ class Simulator {
     size_t container_height_;
 
     /**
-     * Generates a random double between 0 and a given maximum value.
+     * Generates a random double between a given min and max value.
      *
      * @param max_value the max value to generate.
+     * @param min_value the min value to generate.
      *
      * @return a random double in the given range.
      */
-    double GenerateRandomDouble(double max_value) const;
+    double GenerateRandomDouble(double max_value, double min_value) const;
 };
 
 } // namespace visualizer
--- src/simulator.cc
+++ src/simulator.cc
@@ -15,18 +15,22 @@ Simulator::Simulator(const vec2 &top_left_corner, size_t number_particles,
   container_width_ = container_width;
   container_height_ = container_height;
 
+  //generate given number of random particles
   size_t max_x_position = container_width_ - particle_radius * 2;
   size_t max_y_position = container_height_ - particle_radius * 2;
+  int max_velocity_magnitude = particle_radius / 2;
 
   for (int index = 0; index < number_particles; index++) {
     //generate random position
-    double x_position = GenerateRandomDouble((double) max_x_position);
-    double y_position = GenerateRandomDouble((double) max_y_position);
+    double x_position = GenerateRandomDouble((double) max_x_position, 0.0);
+    double y_position = GenerateRandomDouble((double) max_y_position, 0.0);
     vec2 particle_position(x_position, y_position);
 
     //generate random velocity
-    double x_velocity = GenerateRandomDouble((double) (particle_radius / 2));
-    double y_velocity = GenerateRandomDouble((double) (particle_radius / 2));
+    double x_velocity = GenerateRandomDouble((double) max_velocity_magnitude,
+                                             (double) -max_velocity_magnitude);
+    double y_velocity = GenerateRandomDouble((double) max_velocity_magnitude,
+                                             (double) -max_velocity_magnitude);
     vec2 particle_velocity(x_velocity, y_velocity);
 
     //add new particle to vector of particles
@@ -36,14 +40,19 @@ Simulator::Simulator(const vec2 &top_left_corner, size_t number_particles,
 }
 
 void Simulator::Update() {
-  //TODO: implement update
+  for (Particle& particle: particles_) {
+    //check if collision happens
+
+
+    //update particle's position
+    particle.position += particle.velocity;
+  }
 }
 
 void Simulator::Draw() const {
   //draw rectangular container for particles
   vec2 pixel_bottom_right = top_left_corner_ +
                             vec2(container_width_, container_height_);
-
   ci::Rectf container_box(top_left_corner_, pixel_bottom_right);
   ci::gl::color(ci::Color("white"));
   ci::gl::drawStrokedRect(container_box);
@@ -57,8 +66,8 @@ void Simulator::Draw() const {
   }
 }
 
-double Simulator::GenerateRandomDouble(double max_value) const {
-  return max_value * (double) rand() / (double) RAND_MAX;
+double Simulator::GenerateRandomDouble(double max_value, double min_value) const {
+  return (max_value - min_value) * (double) rand() / (double) RAND_MAX;
 }
 
 } // namespace visualizer
